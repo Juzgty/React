e36deb4e669dfc4ac3bdca8f1d07941e028b9c38
Juzgty <14936228+juzgty@user.noreply.gitee.com>
2026-01-12 22:29:54 +0800

    新增React学习笔记

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const list = [
+  {id: 1001,name: 'vue'},
+  {id: 1002,name: 'React'},
+  {id: 1003,name: 'Angular'}
+]
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        this is my first react app
+        {/*渲染列表*/}
+        {list.map(item => (
+          <div key={item.id}>{item.name}</div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
